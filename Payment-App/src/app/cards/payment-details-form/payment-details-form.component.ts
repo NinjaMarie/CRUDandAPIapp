5750504f6746e826687852ee3a53c1f8762ec23f
NinjaMarie <ninamarie.mamongay9@gmail.com>
2024-02-23 16:34:05 +0800

    features: Main Page

--- Payment-App/src/app/cards/payment-details-form/payment-details-form.component.ts
+++ Payment-App/src/app/cards/payment-details-form/payment-details-form.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PaymentDetailsService } from 'src/app/shared/services/payment-details.service';
 
 @Component({
   selector: 'app-payment-details-form',
   templateUrl: './payment-details-form.component.html',
-  styleUrls: ['./payment-details-form.component.css']
+  styleUrls: ['./payment-details-form.component.css'],
 })
-export class PaymentDetailsFormComponent {
+export class PaymentDetailsFormComponent implements OnInit {
+  constructor(public service: PaymentDetailsService) {}
+  ngOnInit(): void {
 
+    this.service.
+  }
 }
--- Payment-App/src/app/shared/services/payment-details.service.ts
+++ Payment-App/src/app/shared/services/payment-details.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment.development';
+import { PaymentDetail } from '../model/paymentDetail-model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PaymentDetailsService {
   url: string = environment.apiBaseUrl + '/PaymentDetail';
+  list: PaymentDetail[] = [];
+  formData: PaymentDetail = new PaymentDetail();
   constructor(private http: HttpClient) {}
 
   refreshList() {
     this.http.get(this.url).subscribe({
       next: (res) => {
         console.log(res);
+        this.list = res as PaymentDetail[];
       },
       error: (err) => {},
     });
